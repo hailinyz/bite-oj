b8670b8d18cddd7c83cbcdaf85844cdfafaee21a
hailinyz <1660246929@qq.com>
2025-12-14 20:47:27 +0800

    竞赛管理-题目列表

--- oj-modules/oj-system/src/main/java/com/bite/system/domain/exam/vo/ExamVO.java
+++ oj-modules/oj-system/src/main/java/com/bite/system/domain/exam/vo/ExamVO.java
@@ -1,5 +1,6 @@
 package com.bite.system.domain.exam.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,14 +13,17 @@ public class ExamVO {
 
     private String title;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
 
     private Integer status;
 
     private String createName;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
 }
